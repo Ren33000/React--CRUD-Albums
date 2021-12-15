bec144813cab3d335ce255d85604ab8296243c2c
Mylene <pont.mylene@gmail.com>
2021-12-15 17:46:38 +0900

    working on the state to store the API answer

--- src/component/AddAlbums.js
+++ src/component/AddAlbums.js
@@ -1,38 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+const [albums,setAlbums]=useState([])
 
-
-const SaveAlbums = () => {
+const fetchData  = () => {
     fetch('https://jsonplaceholder.typicode.com/users/1/albums', {
-    method: 'POST',
-    body: JSON.stringify({
-        name: 'foo',
+      method: 'POST',
+      body: JSON.stringify({
+          name: 'foo',
     }),
     headers: {
         'Content-type': 'application/json; charset=UTF-8',
     },
     })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+      .then((response) => response.json())
+      .then((json) => console.log(json));
     }
+
 
 // to start from here
 // calling the API and saving it
 // https://jsonplaceholder.typicode.com/guide/   
-  this.handleChange = this.handleChange.bind(this);
-  }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
 
-}
 
 function MyForm() {
   return (
     <form>
       <label>Enter your album's name:
-        <input type="text" value={this.state.value} onClick= {SaveAlbums} placeholder="Enter Album's name" />
+        <input type="text" value={this.state.value} placeholder="Enter Album's name" />
         <button href="#text-buttons">Add</button>
       </label>
     </form>
